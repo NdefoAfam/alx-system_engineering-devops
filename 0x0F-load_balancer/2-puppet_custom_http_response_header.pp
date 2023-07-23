c8b709619124c0bfc29c7fbe57bea87621d09517
Ndefo Afam <ndefoafam@gmail.com>
2023-07-23 20:15:29 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,19 @@
 # Use Puppet to automate the task of creating a custom HTTP header response
 
-exec {'update':
+exec { 'update':
   command => '/usr/bin/apt-get update',
 }
--> package {'nginx':
+-> package { 'nginx':
   ensure => 'present',
 }
 -> file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";\n",
   match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+  require => Package['nginx'],  # Add this line to ensure the package 'nginx' is installed before making changes to the file.
 }
--> exec {'run':
+-> exec { 'run':
   command => '/usr/sbin/service nginx restart',
+  require => File_line['http_header'],  # Add this line to ensure the file_line resource is applied before restarting Nginx.
 }
+
